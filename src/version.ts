1d2c3299ca1efbe458508837d06be9460af53367
Miles Burton <miles@milesburton.com>
2025-12-07 19:39:12 +0000

    chore(husky): add commit-msg hook to enforce conventional commits via commitlint

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Auto-generated file. Do not edit.
-export const APP_VERSION = '0.0.31';
-export const APP_BUILD_DATE = '2025-12-07T19:29:44Z';
+export const APP_VERSION = '0.0.32';
+export const APP_BUILD_DATE = '2025-12-07T19:30:38Z';
