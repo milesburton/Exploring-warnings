b2415e735d13a5b9350034674bd83ee1eaee79a9
Miles Burton <miles@milesburton.com>
2025-12-07 18:43:03 +0000

    chore(husky): remove deprecated sourcing from pre-commit for v10 readiness

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Auto-generated file. Do not edit.
-export const APP_VERSION = '0.0.7';
-export const APP_BUILD_DATE = '2025-12-07T18:38:02Z';
+export const APP_VERSION = '0.0.8';
+export const APP_BUILD_DATE = '2025-12-07T18:41:25Z';
