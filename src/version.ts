97fda1cc4fd29d14bebf8fcc42289c3b213b1c93
Miles Burton <miles@milesburton.com>
2025-12-07 19:30:38 +0000

    chore: minor cleanup

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Auto-generated file. Do not edit.
-export const APP_VERSION = '0.0.30';
-export const APP_BUILD_DATE = '2025-12-07T19:25:49Z';
+export const APP_VERSION = '0.0.31';
+export const APP_BUILD_DATE = '2025-12-07T19:29:44Z';
