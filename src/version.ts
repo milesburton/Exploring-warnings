9b939c690f81ef7a7304c3c76366f684ba9ae74e
Miles Burton <miles@milesburton.com>
2025-12-07 18:52:56 +0000

    test(e2e): refresh snapshots after tooltip placement/styling changes

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Auto-generated file. Do not edit.
-export const APP_VERSION = '0.0.13';
-export const APP_BUILD_DATE = '2025-12-07T18:49:58Z';
+export const APP_VERSION = '0.0.14';
+export const APP_BUILD_DATE = '2025-12-07T18:52:18Z';
