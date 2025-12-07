60a7591582a83d32e4c84f022982897acc722e91
Miles Burton <miles@milesburton.com>
2025-12-07 19:29:44 +0000

    test: refresh Playwright snapshots after SCSS tidy

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Auto-generated file. Do not edit.
-export const APP_VERSION = '0.0.25';
-export const APP_BUILD_DATE = '2025-12-07T19:18:36Z';
+export const APP_VERSION = '0.0.30';
+export const APP_BUILD_DATE = '2025-12-07T19:25:49Z';
