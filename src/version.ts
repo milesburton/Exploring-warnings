a747c282b615d59e0c86617ec0225a3760ed5a50
Miles Burton <miles@milesburton.com>
2025-12-07 19:04:16 +0000

    style(top-bar): use surface tokens for alert icon button background/border in light/dark

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Auto-generated file. Do not edit.
-export const APP_VERSION = '0.0.19';
-export const APP_BUILD_DATE = '2025-12-07T19:00:30Z';
+export const APP_VERSION = '0.0.20';
+export const APP_BUILD_DATE = '2025-12-07T19:02:06Z';
