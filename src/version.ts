1dd643433133a8e7ba42b4592ecf47ad4bf0c06e
Miles Burton <miles@milesburton.com>
2025-12-07 19:55:28 +0000

    style(global): merge duplicate .p-popover selector
    test(e2e): update snapshots for 560px overlay

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Auto-generated file. Do not edit.
-export const APP_VERSION = '0.0.38';
-export const APP_BUILD_DATE = '2025-12-07T19:47:13Z';
+export const APP_VERSION = '0.0.41';
+export const APP_BUILD_DATE = '2025-12-07T19:52:00Z';
