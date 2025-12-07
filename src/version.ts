92f00a74f4cb486c9f231f855d3f30728161cc58
Miles Burton <miles@milesburton.com>
2025-12-07 18:37:24 +0000

    test(e2e): refresh snapshots after auto version bump (footer date)

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Auto-generated file. Do not edit.
-export const APP_VERSION = '0.0.4';
-export const APP_BUILD_DATE = '2025-12-07T18:36:03Z';
+export const APP_VERSION = '0.0.5';
+export const APP_BUILD_DATE = '2025-12-07T18:36:46Z';
