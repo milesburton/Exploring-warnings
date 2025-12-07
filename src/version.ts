cc8fa5a0619121a2fa09b4bda5b323ec7e42f5e3
Miles Burton <miles@milesburton.com>
2025-12-07 19:02:06 +0000

    style(top-bar): make warning icon color theme-aware using tokens

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Auto-generated file. Do not edit.
-export const APP_VERSION = '0.0.18';
-export const APP_BUILD_DATE = '2025-12-07T18:58:38Z';
+export const APP_VERSION = '0.0.19';
+export const APP_BUILD_DATE = '2025-12-07T19:00:30Z';
