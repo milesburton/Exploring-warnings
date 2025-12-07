6b4e7cbfd21723cd9e1ac6ffd83a8e9c7ba99c5b
Miles Burton <miles@milesburton.com>
2025-12-07 19:44:06 +0000

    chore: remove temp commitlint test file

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Auto-generated file. Do not edit.
-export const APP_VERSION = '0.0.34';
-export const APP_BUILD_DATE = '2025-12-07T19:39:12Z';
+export const APP_VERSION = '0.0.36';
+export const APP_BUILD_DATE = '2025-12-07T19:41:38Z';
