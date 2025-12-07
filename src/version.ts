9d9a921dfd916242e0935c0b87de8e5d8ca96065
Miles Burton <miles@milesburton.com>
2025-12-07 19:56:35 +0000

    chore: lockfile sync

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Auto-generated file. Do not edit.
-export const APP_VERSION = '0.0.42';
-export const APP_BUILD_DATE = '2025-12-07T19:55:28Z';
+export const APP_VERSION = '0.0.43';
+export const APP_BUILD_DATE = '2025-12-07T19:56:14Z';
